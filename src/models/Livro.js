53008717a38b07734a4ff6e196fa1d9108f8f341
CaioQC <caioq2004@gmail.com>
2025-07-02 20:03:24 -0300

    Buscas por titulo, nome do autor e número de páginas. Regex aplicado nos filtros

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -16,7 +16,15 @@ const livroSchema = new mongoose.Schema(
             type: String,
             required: [true, "O nome da editora é obrigatório"]
         },
-        numeroPaginas: {type: Number}
+        numeroPaginas: {
+            type: Number,
+            validate: {
+                validator : (valor)=>{
+                return valor >= 10 && valor <= 5000
+                },
+                message : "O número de páginas deve estar entre 10 e 5000. Valor fornecido {VALUE}"
+            }
+        }
     }
 );
 
